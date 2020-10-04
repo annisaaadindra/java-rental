6ad5a8f1c91e440545bc1319f62d3ddd3f7cd499
annisaaadindra <annisaaadindra@gmail.com>
2020-10-04 22:56:24 +0700

    initialized

---
+++ src/Anggota.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+
+public class Anggota {
+    private String nomor;
+    private String nama;
+    private String alamat;
+    private ArrayList daftarPinjaman;// unknown usage
+
+    public Anggota() {
+        daftarPinjaman = new ArrayList();
+    }
+
+    public Anggota(String nomor, String nama, String alamat) {
+        this.alamat = alamat;
+        this.nama = nama;
+        this.nomor = nomor;
+        daftarPinjaman = new ArrayList();
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public ArrayList getDaftarPinjaman() {
+        return daftarPinjaman;
+    }
+
+    public void setDaftarPinjaman(ArrayList daftarPinjaman) {
+        this.daftarPinjaman = daftarPinjaman;
+    }
+
+    public void pinjam(Buku buku) {
+        this.daftarPinjaman.add(buku);
+    }
+
+    public Buku kembalikan(Buku buku) {
+        if (this.daftarPinjaman.contains(buku)) {
+            this.daftarPinjaman.remove(buku);
+            return buku;
+        }
+
+        else {
+            return null;
+        }
+    }
+
+    public Buku searchBuku(String kode)
+    {
+        Buku temp = null;
+        for(int x=0; x<this.daftarPinjaman.size();x++)
+        {
+            temp = (Buku)this.daftarPinjaman.get(x); //uncknown usage
+            if(temp.getKode().equalsIgnoreCase(kode))
+                break;
+        }
+
+        return temp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNomor() {
+        return nomor;
+    }
+
+    public void setNomor(String nomor) {
+        this.nomor = nomor;
+    }
+}
